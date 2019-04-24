5059d0ed41759de6b36b8c017b35331f8069075f
edimossilva <edimossilva@gmail.com>
2019-04-24 09:21:25 -0300

    Adding update and refactoring

--- app/controllers/api/v1/pictures_controller.rb
+++ app/controllers/api/v1/pictures_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::PicturesController < ApplicationController
   def create
     picture = Picture.new create_picture_params
-    picture.image.attach create_picture_params[:image]
+    picture.image.attach create_picture_params[:image] if create_picture_params[:image]
     if picture.save
-      render json: picture, status: :ok, serializer: PictureSerializer
+      render_picture(picture, :created)
     else
       render json: { errors: picture.errors }, status: :unprocessable_entity
     end
@@ -13,9 +13,38 @@ class Api::V1::PicturesController < ApplicationController
     render json: Picture.all , each_serializer: PictureSerializer
   end
 
+  def update
+    picture = Picture.find_by_id update_picture_params[:id]
+    if picture
+      picture.image.attach update_picture_params[:image] if update_picture_params[:image]
+      picture.name = update_picture_params[:name]
+      if picture.save
+        render_picture(picture, :ok)
+      else
+        render json: { errors: picture.errors }, status: :unprocessable_entity
+      end
+    else
+      render_picture_not_found
+    end
+  end
+
+
   private
 
   def create_picture_params
     params.permit(:name, :image)
   end
+
+  def update_picture_params
+    params.permit(:id, :name, :image)
+  end
+
+  def render_picture(picture, status)
+    render json: picture, status: status, serializer: PictureSerializer
+  end
+
+  def render_picture_not_found
+    render json: { message: "not found"}, status: :not_found
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :pictures, only: %i[index create]
+      resources :pictures, only: %i[index create update]
     end
   end
 end
