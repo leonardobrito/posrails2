dcea7ac666a69f8b7336fa9a4e70a6cfc9e38323
Edimo Silva <edimo.silva@redealumni.com>
2019-04-27 16:33:18 -0300

    Using local sotrage

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,10 +1,9 @@
 include Rails.application.routes.url_helpers
 class Picture < ApplicationRecord
-
   validates :name, presence: true, uniqueness: true
   has_one_attached :image
 
   def image_url
-    image.service_url.split("?").first
+    url_for(image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  default_url_options :host => "localhost:3000"
+
   namespace :api do
     namespace :v1 do
       resources :pictures, only: %i[index create update]
